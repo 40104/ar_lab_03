2062bbb35b03e06242dca31a587bd0d86fd6e458
40104 <danile12333212@gmail.com>
2022-05-05 22:41:29 +0000

    Commit

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-sudo ./build/ar_lab_01 --host=localhost --port=3306 --login=stud --password=stud --database=stud --init_db 
+sudo ./build/ar_lab_03 --host=127.0.0.1 --port=3306 --login=stud --password=stud --database=stud --init_db --cache_servers='127.0.0.1:10800,127.0.0.1:10900'
 
